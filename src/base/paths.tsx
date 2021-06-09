417fa5943cd6e1ec7fd8a4e1ff86d8f7f006a3d8
ingrqm <contact@imaxart.com>
2021-06-09 10:17:24 +0200

    feat: config plugins and create structure

---
+++ src/base/paths.tsx
@@ -0,0 +1,13 @@
+const API_URL = process.env.REACT_APP_API_URL;
+
+export const appUrls = {
+  portal: {
+    landing: '/',
+  },
+};
+
+export const apiUrls = {
+  portal: {
+    users: `${API_URL}/users`,
+  },
+};
